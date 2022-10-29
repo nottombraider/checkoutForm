d94dba81b750220d957731c4734797d1e38660d4
nottombraider <larysa.yavorska@outlook.com>
2022-10-29 13:53:32 +0300

    [feat] - add tailwind setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { setup } from "twind/shim";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const appContainer = document.getElementById('root')
+
+if (!appContainer) throw new Error("app container is missing")
+
+setup({
+    target: appContainer,
+})
+
+ReactDOM.createRoot(appContainer).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
