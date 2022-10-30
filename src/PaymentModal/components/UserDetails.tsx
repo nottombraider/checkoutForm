cb0d8b7028c3e04cf8c0a137a2e2da0db1dbd692
nottombraider <larysa.yavorska@outlook.com>
2022-10-30 17:59:24 +0200

    [feat] - add validation to form

--- src/PaymentModal/components/UserDetails.tsx
+++ src/PaymentModal/components/UserDetails.tsx
@@ -1,16 +1,21 @@
-import { Form } from "../../shared";
-import { FormBody } from "../FormSteps";
 import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Form } from "../../shared";
+import { FormBody, paymentMethodValidationSchema } from "../FormSteps";
 
-export const UserDetails = (): JSX.Element => (
-  <main className="h-full overflow-auto scrollbar md:pr-4 rounded w-full md:w-[fit-content]">
-    <Form
-      onSubmit={() => (formValues) => {
-        console.log("formValues:", formValues);
-      }}
-      className="h-full rounded"
-    >
-      <FormBody />
-    </Form>
-  </main>
-);
+export const UserDetails = (): JSX.Element => {
+  return (
+    <main className="h-full overflow-auto scrollbar md:pr-4 rounded w-full md:w-[fit-content]">
+      <Form
+        resolver={yupResolver(paymentMethodValidationSchema)}
+        onSubmit={() => (formValues) => {
+          console.log("formValues:", formValues);
+        }}
+        mode="onChange"
+        className="h-full rounded"
+      >
+        <FormBody />
+      </Form>
+    </main>
+  );
+};
