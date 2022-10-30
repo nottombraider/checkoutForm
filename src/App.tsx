9e8a0392131568d0fa43fd6a811ed357cf791845
nottombraider <larysa.yavorska@outlook.com>
2022-10-30 14:37:25 +0200

    [feat] - place modal into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import "animate.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Button } from "./shared";
+import { PaymentModal } from "./PaymentModal/PaymentModal";
+import { useBoolean } from "react-hanger/array";
+
+const App = (): JSX.Element => {
+  const [isModalOpen, { toggle }] = useBoolean(false);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <section className="p-10">
+      <Button onClick={toggle} className="px-6">
+        Pay
+      </Button>
+      {isModalOpen && <PaymentModal onClose={toggle} />}
+    </section>
+  );
+};
 
-export default App
+export default App;
