a583059c20d794cd9f2bd8fea65c2a958ed5e52b
nottombraider <larysa.yavorska@outlook.com>
2022-10-30 17:58:51 +0200

    [refactor] - update error state styles

--- src/shared/components/Form/fields/styles.ts
+++ src/shared/components/Form/fields/styles.ts
@@ -10,13 +10,16 @@ export const sharedFieldStyles = (
   classnames(
     "w-full border border-gray-200 outline-none focus:ring-indigo-300 focus:ring-2 rounded py-1 px-3 text-xs",
     { "pl-6": isIcon },
-    { "border-red-500 focus:border-indigo-300": isError },
+    {
+      "border-red-500 focus:border-red-500 focus:ring-red-500 focus:ring-1 animate__animated animate__shakeX":
+        isError,
+    },
     { "focus:border-green-300 focus:ring-green-300 focus:ring-1": isValid }
   );
 
 export const errorMessage = "text-xs text-red-600";
 
-export const fieldIcon = "w-3 my-1 mx-2 absolute";
+export const fieldIcon = "w-3 my-1 mx-2 absolute z-10";
 
 export const endIconIdentifier =
-  "absolute right-2 top-1 animate__animated animate__zoomIn animate__delay-1s";
+  "absolute right-2 top-1 animate__animated animate__zoomIn";
